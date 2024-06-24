25519b2a0d9f1480742b7ab503962e6606b4f803
João Neto <1539767+joaomlneto@users.noreply.github.com>
2024-06-24 09:57:26 +0200

    bootstrap cobra CLI library

--- cmd/dstest/main.go
+++ cmd/dstest/main.go
@@ -1,30 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"log"
-
-	"github.com/egeberkaygulcan/dstest/cmd/dstest/config"
-	"github.com/egeberkaygulcan/dstest/cmd/dstest/engine"
-)
+import "github.com/egeberkaygulcan/dstest/cmd/dstest/cmd"
 
 func main() {
-
-	// ------ DO NOT CHANGE -------
-
-	fmt.Println("Starting dstest")
-	// Read config
-	cfg, err := config.Read()
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	// -----------------------------
-
-	fmt.Println("Name: " + cfg.TestConfig.Name)
-
-	te := new(engine.TestEngine)
-	te.Init(cfg)
-
-	te.Run()
+	cmd.Execute()
 }
