597ac6202729df96bcb2291fd5c7f6153e6aa536
Ege Berkay Gulcan <egeberkaygulcan@gmail.com>
2024-07-06 13:04:17 +0200

    Fixed Zookeeper script

--- scripts/zk_clean.sh
+++ scripts/zk_clean.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 BIN=../../../zookeeper
-for dir in $BIN/conf/dstest/*/     # list directories in the form "/tmp/dirname/"
-do
-    dir=${dir%*/}      # remove the trailing "/"
-    $BIN/bin/zkServer.sh --config $dir stop
-done
+# for dir in $BIN/conf/dstest/*/     # list directories in the form "/tmp/dirname/"
+# do
+#     dir=${dir%*/}      # remove the trailing "/"
+#     $BIN/bin/zkServer.sh --config $dir stop
+# done
 
-rm -rf {$BIN}/conf/dstest
+rm -rf zk_conf
 rm -rf /tmp/zookeeper
--- scripts/zk_server.sh
+++ scripts/zk_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 BIN=../../../zookeeper
-mkdir -p $BIN/conf/dstest/zoo_$5
-printf "tickTime=2000\ndataDir=/tmp/zookeeper/zk_$5\nclientPort=$4\ninitLimit=5\nsyncLimit=2\nserver.1=localhost:2888:$1\nserver.2=localhost:2889:$2\nserver.3=localhost:2890:$3" >> ${BIN}/conf/dstest/zoo_$5/zoo.cfg
+mkdir -p zk_conf/zoo_$5
+printf "tickTime=2000\ndataDir=/tmp/zookeeper/zk_$5\nclientPort=$4\ninitLimit=5\nsyncLimit=2\nserver.1=localhost:2888:$1\nserver.2=localhost:2889:$2\nserver.3=localhost:2890:$3" >> zk_conf/zoo_$5/zoo.cfg
 mkdir -p /tmp/zookeeper/zk_$5
 printf $5 >> /tmp/zookeeper/zk_$5/myid
-bash ${BIN}/bin/zkServer.sh --config ${BIN}/conf/dstest/zoo_$5 start
+${BIN}/bin/zkServer.sh --config zk_conf/zoo_$5 start-foreground
